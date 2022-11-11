a3d816c923d53f8a7189866826d6b22ed2863a5f
Rogger Neira <rknc.neira@gmail.com>
2022-11-10 23:11:35 -0500

    Ayuda

---
+++ app/src/main/java/com/example/alahuaca/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.alahuaca
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
+        var btningresar : Button = findViewById(R.id.btnIngresar)
+        val txtemail : TextView = findViewById(R.id.edtEmail)
+        val password : TextView = findViewById(R.id.edtPassword)
+        val btnregistrar : Button = findViewById(R.id.btnRegistrar)
+        firebaseAuth = Firebase.auth
+        btningresar.setOnClickListener(){
+            ingresar(txtemail.text.toString(),password.text.toString())
+        }
+        btnregistrar.setOnClickListener(){
+            val i = Intent(this, Registrar::class.java)
+            startActivity(i)
+        }
+    }
+    private fun ingresar(email: String, password: String){
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+            task ->
+            if(task.isSuccessful){
+                val user = firebaseAuth.currentUser
+                Toast.makeText(baseContext, "Loading", Toast.LENGTH_SHORT).show()
+                val i = Intent(this, Scrolling::class.java)
+                startActivity(i)
+            }
+            else{
+                Toast.makeText(baseContext,"Correo y/o contraseña incorrecta", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
